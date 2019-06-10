f200bb1e49a487bec86cb8ad0146c07f0afa7a75
JM_LIU <33569853+676763570@users.noreply.github.com>
2019-06-10 22:15:42 +0800

    -

--- request edtion/test.py
+++ request edtion/test.py
@@ -1,4 +1,5 @@
 from defs import *
+fron send_email import *
 import json
 import time
 
@@ -19,8 +20,8 @@ def Load_data():
     return User_data,Completed,Wait_q
 
 try:
-	print('loading data')
 	User_data,Completed,Wait_q = Load_data()
+	print('load data')
 except:
 	Fir_token = 'TechMonster'
 	User_data = {}
@@ -29,17 +30,22 @@ except:
 	wait_q.append(Fir_token)
 
 while True:
-	token = Wait_q[0]
 	start_t = time.time()
+	try:
+		token = Wait_q[0]
+		url = 'https://www.zhihu.com/people/'+token
+		user,res = Prase_user(url,1,act_limit=1000)
+		User_data[user] = res
+		for u in User_data[user]['following']:
+			if u['url_token'] not in Wait_q and u['url_token'] not in Completed:
+				Wait_q.append(u['url_token'])
+		del Wait_q[0]
+		Completed.append(user)
 
-	url = 'https://www.zhihu.com/people/'+token
-	user,res = Prase_user(url,1,act_limit=1000)
-	User_data[user] = res
-	for u in User_data[user]['following']:
-		if u['url_token'] not in Wait_q and u['url_token'] not in Completed:
-			Wait_q.append(u['url_token'])
-	del Wait_q[0]
-	Completed.append(user)
+	except Exception as e:
+		print(e)
+		send_email()
+		pass
 
 	end_t = time.time()
 	print(len(Completed),round((end_t-start_t)/60,2))
